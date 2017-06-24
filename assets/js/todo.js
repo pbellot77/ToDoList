564ae6da570942ecbf8ac36b2a49b0c37a40e1df
Patrick <patrickbellot77@gmail.com>
2017-06-23 20:56:22 -0500

    adding js

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,21 +1,26 @@
 // Check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
 
+//add li upon enter from text input
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var todoText = $(this).val();
 		$(this).val("");
 		// create a new li and add to ul
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
 	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
